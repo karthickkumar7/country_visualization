582ab8cd4f123a1b7058fea23d9c0943a8626250
karthick <karthickkumar7.k7@gmail.com>
2023-09-06 22:18:57 -0700

    changed table to shadcn lib

--- src/components/countries/CountryTable.tsx
+++ src/components/countries/CountryTable.tsx
@@ -4,6 +4,15 @@ import { inSelected, parseArea } from '@/utils/countries';
 import { useSelector, useDispatch } from 'react-redux';
 import { addToSelectedCountry } from '@/redux/countrySlice';
 import { Country } from '@/types/types';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
 const CountryTable = () => {
     const { countries, selectedCountries } = useSelector(
@@ -15,56 +24,61 @@ const CountryTable = () => {
         dispatch(addToSelectedCountry(country));
     };
 
-    return (
-        <table className="w-full">
-            <thead className="w-full">
-                <tr className="w-full px-1 flex justify-between capitalize text-sm md:text-xl rounded-t duration-300 bg-blue-600 text-white">
-                    <th className="w-[5%] h-full py-4 px-1 hidden md:inline-flex">
-                        no
-                    </th>
-                    <th className="w-[20%] h-full py-4">name</th>
-                    <th className="w-[20%] h-full py-4">gpd (nom) (bil)</th>
-                    <th className="w-[15%] h-full py-4">gdp (ppp) (bil)</th>
-                    <th className="w-[20%] h-full py-4">
-                        area (km<sup>2</sup>)
-                    </th>
-                    <th className="w-[20%] h-full py-4">population (mil)</th>
-                </tr>
-            </thead>
+    const tableHeaderStyle =
+        'font-semibold uppercase text-white cursor-default';
 
-            <tbody className="w-full">
+    return (
+        <Table className="">
+            <TableHeader className="">
+                <TableRow className="bg-blue-600 hover:bg-blue-600">
+                    <TableHead className={`${tableHeaderStyle}`}>no</TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        name
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        gpd (nom) (bil)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        gdp (ppp) (bil)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        area (km<sup>2</sup>)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        population (mil)
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody className="w-full">
                 {countries.map((country, i) => (
-                    <tr
+                    <TableRow
                         key={country.name}
-                        className={`w-full px-1 py-3 md:py-2 flex justify-between text-xs md:text-lg font-semibold cursor-pointer border ${
+                        className={`cursor-pointer ${
                             inSelected(country, selectedCountries)
-                                ? 'bg-blue-100'
+                                ? 'bg-blue-100 hover:bg-blue-100'
                                 : 'hover:bg-slate-100'
                         }`}
                         onClick={() => selectCountry(country)}
                     >
-                        <td className="w-[5%] hidden md:inline-flex">
-                            {i + 1}
-                        </td>
-                        <td className="w-[20%] px-2 capitalize">
-                            {country.name}
-                        </td>
-                        <td className="w-[20%] px-2 capitalize">
+                        <TableCell className="">{i + 1}</TableCell>
+                        <TableCell className="">{country.name}</TableCell>
+                        <TableCell className="">
                             {parseArea(country.gdp_nominal)}
-                        </td>
-                        <td className="w-[15%] px-2 md:px-4">
+                        </TableCell>
+                        <TableCell className="">
                             {parseArea(country.gdp_ppp)}
-                        </td>
-                        <td className="w-[20%] px-2">
+                        </TableCell>
+                        <TableCell className="">
                             {parseArea(country.area)}
-                        </td>
-                        <td className="w-[20%] px-4">
+                        </TableCell>
+                        <TableCell className="">
                             {(country.population / 1e6).toFixed(2)}
-                        </td>
-                    </tr>
+                        </TableCell>
+                    </TableRow>
                 ))}
-            </tbody>
-        </table>
+            </TableBody>
+        </Table>
     );
 };
 
--- src/components/india/IndiaTable.tsx
+++ src/components/india/IndiaTable.tsx
@@ -4,6 +4,14 @@ import store, { RootState } from '@/redux/store';
 import { India } from '@/types/types';
 import { inSelected, parseArea } from '@/utils/countries';
 import { useSelector } from 'react-redux';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
 
 const IndiaTable = () => {
     const { india, selectedStates } = useSelector(
@@ -14,60 +22,65 @@ const IndiaTable = () => {
         store.dispatch(addToSelectedStates(state));
     };
 
-    return (
-        <table className="w-full mt-4">
-            <thead className="w-full">
-                <tr className="w-full px-1 flex justify-between capitalize text-sm md:text-xl rounded-t duration-300 bg-blue-600 text-white">
-                    <th className="w-[5%] h-full py-4 px-1 hidden md:inline-flex">
-                        no
-                    </th>
-                    <th className="w-[20%] h-full py-4">name</th>
-                    <th className="w-[20%] h-full py-4">abbr</th>
-                    <th className="w-[20%] h-full py-4">gpd (bil)</th>
-                    <th className="w-[20%] h-full py-4">gpd pc (bil)</th>
-                    <th className="w-[20%] h-full py-4">
-                        area (km<sup>2</sup>)
-                    </th>
-                    <th className="w-[20%] h-full py-4">population (mil)</th>
-                </tr>
-            </thead>
+    const tableHeaderStyle =
+        'font-semibold uppercase cursor-default text-white';
 
-            <tbody className="w-full">
+    return (
+        <Table className="mt-[13px]">
+            <TableHeader className="">
+                <TableRow className="lg:w-[1200px] bg-blue-600 hover:bg-blue-600">
+                    <TableHead className={`${tableHeaderStyle}`}>no</TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        name
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        abbr
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        gpd (bil)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        gpd pc (bil)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        area (km<sup>2</sup>)
+                    </TableHead>
+                    <TableHead className={`${tableHeaderStyle}`}>
+                        population (mil)
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody className="">
                 {india.map((state, i) => (
-                    <tr
+                    <TableRow
                         key={state.name}
-                        className={`w-full px-1 py-3 md:py-2 flex justify-between text-xs md:text-lg font-semibold cursor-pointer border ${
+                        className={`cursor-pointer ${
                             inSelected(state, selectedStates)
-                                ? 'bg-blue-100'
+                                ? 'bg-blue-100 font-semibold hover:bg-blue-100'
                                 : 'hover:bg-slate-100'
                         }`}
                         onClick={() => selectState(state)}
                     >
-                        <td className="w-[5%] hidden md:inline-flex">
-                            {i + 1}
-                        </td>
-                        <td className="w-[20%] pl-2 capitalize">
-                            {state.fullname}
-                        </td>
-                        <td className="w-[5%] pl-2 md:pl-12 capitalize">
-                            {state.name}
-                        </td>
-                        <td className="w-[15%] pl-8 md:pl-28 capitalize">
+                        <TableCell className="">{i + 1}</TableCell>
+                        <TableCell className="">{state.fullname}</TableCell>
+                        <TableCell className="">{state.name}</TableCell>
+                        <TableCell className="">
                             {parseArea(state.gdp)}
-                        </td>
-                        <td className="w-[20%] pl-8 md:pl-24 capitalize">
+                        </TableCell>
+                        <TableCell className="">
                             {parseArea(state.gdp_pc)}
-                        </td>
-                        <td className="w-[20%] pl-4 md:px-24">
+                        </TableCell>
+                        <TableCell className="">
                             {parseArea(state.area)}
-                        </td>
-                        <td className="w-[20%] px-4 md:px-12">
+                        </TableCell>
+                        <TableCell className="">
                             {(state.population / 1e6).toFixed(2)}
-                        </td>
-                    </tr>
+                        </TableCell>
+                    </TableRow>
                 ))}
-            </tbody>
-        </table>
+            </TableBody>
+        </Table>
     );
 };
 
